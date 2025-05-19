d02301eea43413d64ac6cae6fb8dfeaf67a0883a
あすぱる <contact@aspulse.dev>
2025-05-19 09:26:46 +0900

    chore: Support React 19 (#28)

--- tests/sandbox/eslint.config.js
+++ tests/sandbox/eslint.config.js
@@ -1,28 +1,19 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import libraryConfig from '@repo/eslint-config/library.js';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  ...libraryConfig,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ['**/*.ts?(x)'],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      '@typescript-eslint/no-unused-vars': [
         'warn',
-        { allowConstantExport: true },
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
       ],
     },
-  }
-);
+  },
+];
